590b61017819a31925942c82e15e4a20a014e89a
Mark T. Holder <mtholder@gmail.com>
2014-01-25 20:36:47 -0600

    nexml test

--- nexson-validator/test.sh
+++ nexson-validator/test.sh
@@ -15,9 +15,28 @@ then
     passed=$(expr $passed + 1)
 fi
 
+if ! test -z "${NEXML_SCHEMA}"
+then
+    if test -f "${NEXML_SCHEMA}"
+    then
+        for f in $(ls tests/nexml/*.xml)
+        do
+            total=$(expr $total + 1)
+            if sh scripts/check_nexml_roundtrip.sh "$f" "${NEXML_SCHEMA}" -o
+            then
+                passed=$(expr $passed + 1)
+            fi
+        done
+    else 
+        echo "NeXML roundtrip tests skipped because ${NEXML_SCHEMA} does not exist"
+    fi
+else
+    echo "NeXML roundtrip tests skipped because the NEXML_SCHEMA environmental variable is not set"
+fi
+
 if test $passed -eq $total
 then
     exit 0
 else
     exit 1
-fi
+fi 
