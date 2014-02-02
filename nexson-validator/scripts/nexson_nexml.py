59f5dd9f279fb2a470014baafa6cc3b9ac6ac714
Mark T. Holder <mtholder@gmail.com>
2014-02-01 22:25:16 -0600

    nexson_nexml.py conversion with jx or xj as first argument

---
+++ nexson-validator/scripts/nexson_nexml.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+from nexson_validator import get_ot_study_info_from_nexml, write_obj_as_nexml
+
+if __name__ == '__main__':
+    import sys, codecs, json
+    mode_list = ['xj', 'jx']
+    try:
+        mode = sys.argv[1].lower()
+        assert(mode in mode_list)
+    except:
+        opts = '", "'.join(mode_list)
+        msgf = 'Expecing the first argument to be one of:\n "{o}"'
+        msg = msgf.format(o=opts)
+        sys.exit(msg)
+    try:
+        inp = sys.argv[2]
+    except:
+        inp = sys.stdin
+    out = codecs.getwriter('utf-8')(sys.stdout)
+    
+    if mode == 'xj':
+        o = get_ot_study_info_from_nexml(inp)
+        json.dump(o, out, indent=0, sort_keys=True)
+        out.write('\n')
+    else:
+        o = json.load(codecs.open(inp, 'rU', 'utf-8'))
+        write_obj_as_nexml(o, out)
+
