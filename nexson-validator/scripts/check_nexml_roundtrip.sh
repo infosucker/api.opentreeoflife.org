cd450cd67048cabea58613bb5115902cf700b27f
Mark T. Holder <mtholder@gmail.com>
2014-02-04 14:54:03 -0600

    now requires a validate-nexml execitable on the path to do NeXML validation

--- nexson-validator/scripts/check_nexml_roundtrip.sh
+++ nexson-validator/scripts/check_nexml_roundtrip.sh
@@ -6,65 +6,65 @@ then
     echo "The first arg should be a nexml instance doc. ${inpnexml} does not exist"
     exit 1
 fi
-nexmlschemafp="${2}"
-if ! test -f "${nexmlschemafp}"
+if which validate-nexml >/dev/null
 then
-    echo "Second arg should be the nexml schema. ${nexmlschemafp} does not exist"
-    exit 1
+    do_schema_validation="1"
+else
+    do_schema_validation="0"
+    echo '"validate-nexml" was not found on the path. Validation against schema will be skipped.'
 fi
 
-
-if ! test $3 = "-o"
+if ! test "$2" = "-o"
 then
-    if test -f .1.xml -o -f .1.json -o -f .2.xml -o -f .pp2.xml -o -f .pp2.xml
+    if test -f .1.json -o -f .2.xml -o -f .pp2.xml -o -f .pp2.xml
     then
-        echo "file .# or .pp# files in the way and the -o was not used as the 3rd arg"
+        echo "file .# or .pp# files in the way and the -o was not used as the 2nd arg"
         exit 1
     fi
 fi
 
 # 1. Verify that the input is valid NeXML
 
-# an apparent bug in xmllint
-#   see http://sourceforge.net/mailarchive/message.php?msg_id=31798087
-# necessitates some hacky expunging of attributes from a characters block...
-cat "${inpnexml}" | sed -e 's/<characters\(.*\) xml:base="[^"]*" */<characters\1 /' > .1.xml
-
-inpwasvalid=1
-if ! xmllint --schema "${nexmlschemafp}" .1.xml >/dev/null 2>&1
+if test ${do_schema_validation} -eq "1"
 then
-    echo "${inpnexml} is not a valid NeXML file"
-    inpwasvalid=0
+    inpwasvalid=1
+    if ! validate-nexml "${inpnexml}" >/dev/null 2>&1
+    then
+        echo "${inpnexml} is not a valid NeXML file"
+        inpwasvalid=0
+    fi
 fi
 
 # 2. Convert to JSON
-if ! python "$dir/xml2json-badgerfish.py" nj .1.xml > .1.json
+if ! python "$dir/nexson_nexml.py" "${inpnexml}" .1.json
 then
     echo "Conversion of .1.xml to JSON failed"
     exit 1
 fi
 
 # 3. Convert back to NeXML
-if ! python "$dir/xml2json-badgerfish.py" jn .1.json > .2.xml
+if ! python "$dir/nexson_nexml.py" .1.json .2.xml
 then
     echo "Conversion of .1.json to JSON failed"
     exit 1
 fi
 
 # 4. validate NeXML
-if ! xmllint --schema "${nexmlschemafp}" .2.xml >/dev/null 2>&1
+if test ${do_schema_validation} -eq "1"
 then
-    echo "XML written to .2.xml was not valid NeXML"
-    if test $inpwasvalid -eq 1
+    if ! validate-nexml .2.xml >/dev/null 2>&1
     then
-        exit 1
+        echo "XML written to .2.xml was not valid NeXML"
+        if test $inpwasvalid -eq 1
+        then
+            exit 1
+        fi
     fi
 fi
-
-# 5. very that after pretty printing and culling of unstable aspects of the file
+# 5. verify that after pretty printing and culling of unstable aspects of the file
 # the input and output are identical
 # pretty print
-xmllint --format .1.xml > .pp1.xml || exit
+xmllint --format "${inpnexml}" > .pp1.xml || exit
 xmllint --format .2.xml > .pp2.xml || exit
 
 # pretty print
@@ -82,7 +82,7 @@ then
     exit 1
 fi
 
-if test $3 = "-o"
+if test "${2}" = "-o"
 then
-    rm .1.json .1.xml .2.xml .cpp1.xml .cpp2.xml .pp1.xml .pp2.xml .s1.xml .s2.xml 2>/dev/null
+    rm -f .1.json .2.xml .cpp1.xml .cpp2.xml .pp1.xml .pp2.xml .s1.xml .s2.xml 2>/dev/null
 fi
